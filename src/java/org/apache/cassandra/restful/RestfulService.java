5a2dc38c22a9df7f1575a2c3f1dd35cd83aac4a2
darionyaphets <yaphets.darion@gmail.com>
2013-05-11 23:10:04 +0800

    init restful service

---
+++ src/java/org/apache/cassandra/restful/RestfulService.java
@@ -0,0 +1,15 @@
+package org.apache.cassandra.restful;
+
+public class RestfulService
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/java/org/apache/cassandra/restful/RestfulServiceMBean.java
@@ -0,0 +1,15 @@
+package org.apache.cassandra.restful;
+
+public class RestfulServiceMBean
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
